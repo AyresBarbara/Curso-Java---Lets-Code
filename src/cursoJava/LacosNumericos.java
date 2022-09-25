d002b2d7f180ae54a95b353b9978a1aa7fe9c418
Barbara Ayres <ayresbarbara2@gmail.com>
2022-09-25 15:47:43 -0300

    Laços nUMERICOS

---
+++ src/cursoJava/LacosNumericos.java
@@ -0,0 +1,26 @@
+package cursoJava;
+
+public class LacosNumericos {
+
+	public static void main(String[] args) {
+		// 1 2 3 4 5 6 7 8 9 10
+		// para uma variavel que inicia em 1 e vai até 100, mudando 1 por 1
+		//i++ = mostra de 1 por 1; i+=2 mostra de 2 em 2 
+		for(int i=1; i<=500; i+=2) {
+			
+			System.out.println(1 * i);
+			System.out.println(2 * i);
+			System.out.println(3 * i);
+			System.out.println(4 * i);
+			System.out.println(5 * i);
+			System.out.println(6 * i);
+			System.out.println(7 * i);
+			System.out.println(8 * i);
+			System.out.println(9 * i);
+			System.out.println(10 * i);
+			
+		}
+
+	}
+
+}
---
+++ src/cursoJava/LacosNumericos2.java
@@ -0,0 +1,18 @@
+package cursoJava;
+
+public class LacosNumericos2 {
+
+	public static void main(String[] args) {
+		// 1 2 3 4 5 6 7 8 9 10
+				// para uma variavel que inicia em 1 e vai até 100, mudando 1 por 1
+				//i++ = mostra de 1 por 1; i+=2 mostra de 2 em 2 
+				for(int i=1; i<=10; i++) {
+					for (int j = 1; j<=10; j++) {
+						System.out.println(j + " x " + i + " = " + j * i);
+					}
+					
+				}
+
+			}
+
+	}
