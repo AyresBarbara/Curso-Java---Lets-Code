f601d1861737199d8a7106931c22e15f16e58cda
Barbara Ayres <ayresbarbara2@gmail.com>
2022-09-25 16:34:15 -0300

    Funcoes

---
+++ src/cursoJava/Funcoes.java
@@ -0,0 +1,14 @@
+package cursoJava;
+
+public class Funcoes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome = "Bárbara";
+		saudacao(nome);
+	}
+	public static void saudacao(String nome) {
+		System.out.println("Hello, "+ nome);
+	}
+
+}
---
+++ src/cursoJava/Funcoes2.java
@@ -0,0 +1,14 @@
+package cursoJava;
+
+public class Funcoes2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int resultado = soma( 2, 3);
+		System.out.println(resultado);
+	}
+	public static int soma(int a, int b) {
+		return a+b;
+	}
+
+}
